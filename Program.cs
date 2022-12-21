882fe22056f90477a814eb7ab2a0ade5e4bc340a
Evgeniy <freemen0083@gmail.com>
2022-12-21 22:21:39 +0400

    #1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void PlusElements()
+{
+    Console.WriteLine("Input Massive Length:  ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n <= 0)
+    {
+        Console.WriteLine("Error");
+    }
+    else
+    {
+        double[] arr = new double[n];
+        int colplus = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine("Input element  " + (i + 1));
+            arr[i] = Convert.ToDouble(Console.ReadLine());
+            if (arr[i] > 0)
+            {
+                colplus++;
+            }
+        }
+        Console.WriteLine("Numbers of Plus elements is:  " + colplus);
+    }
+}
+PlusElements();
